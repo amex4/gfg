c4c4883bd96a71b4be886837c721ec9ce9ffcc17
amex4 <74108330+amex4@users.noreply.github.com>
2022-02-02 18:50:15 +0530

    Create matchPairs.py

---
+++ matchPairs.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+class Solution:
+
+    def matchPairs(self,nuts, bolts, n):
+        # code here
+        nuts.sort()
+        bolts.sort()
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        nuts = list(map(str, input().strip().split()))
+        bolts = list(map(str, input().strip().split()))
+        ob = Solution()
+        ob.matchPairs(nuts, bolts, n)
+        for x in nuts:
+            print(x, end=" ")
+        print()
+        for x in bolts:
+            print(x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
